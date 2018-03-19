3107218ce3e41ecd66891ea00df4bdd82f10653d
JJRoss19 <jjross19@gmail.com>
2018-03-19 19:11:04 -0400

    Change JS link

--- projectjavascript.js
+++ projectjavascript.js
@@ -1,3 +1,6 @@
-var numbers= ["seven"];
-document.getElementByClassName("runtime").innerHTML = numbers;
-console.log = "runtime" []
+demoArray = document.getElementById("pft");
+var numbers = [4, 9, 16,];
+
+function myFunction(item, index) {
+    demoArray.innerHTML = demoP.innerHTML + "index" + index + "]: " + item + "<br>";
+}
