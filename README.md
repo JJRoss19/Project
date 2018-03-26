9f73fb051604f67cd96a39f44483f1fdb43238c6
JJRoss19 <jjross19@gmail.com>
2018-03-26 18:48:52 -0400

    changes to readme file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-This is my read me file.
+Project Title
+
+This project was designed to launch a blog around my fitness goal of establishing an USMC Iron man status. Weekly I would report in and provide a summary of how training went durning the week. The front end of the project will utimatley tie into a database with updated proforamce times. Readers will provide email address and contact information for admittance to the site.
+
+Built With:
+
+Built with Visual Studion Code
+
+Authors:
+
+Josh Ross 
+
+
+Acknowledgments:
+
+Code Louisville 
+Code Louisville Mentors 
--- projectjavascript.js
+++ projectjavascript.js
@@ -1,6 +1,16 @@
-demoArray = document.getElementById("pft");
-var numbers = [4, 9, 16,];
+demoArray = document.querySelectorAll (".runners");
+var numbers = [21.31, 18, 66,];
 
-function myFunction(item, index) {
-    demoArray.innerHTML = demoP.innerHTML + "index" + index + "]: " + item + "<br>";
+
+function populateStats (array, values){
+for (var i = 0; i < array.length; i++){
+    array[i].innerHTML = values[i];
 }
+}
+populateStats (demoArray, numbers);
+
+
+
+// function myFunction(item, index) {
+//     demoArray.innerHTML = demoP.innerHTML + "index" + index + "]: " + item + "<br>";
+// }
